f23b30af95c600dc209fa96f4e4ac4b17b0af01c
vladimirivkovic <vlado.ivkovic@live.com>
2018-11-09 09:53:28 +0100

    primer3 updated

--- Vezbe/Termin3/primer3/streaming/commands.sh
+++
@@ -1,3 +0,0 @@
-chmod +x *.py
-
-hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.5.jar -input /sales -output /sales_type -mapper ~/examples/sales/python/mapper.py -reducer ~/examples/sales/python/reducer.py
